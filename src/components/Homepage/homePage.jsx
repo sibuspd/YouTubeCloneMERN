1b963c7ea092a70b74bf3460053d8f164df847a8
sibuspd <sibuspd@gmail.com>
2024-12-09 18:56:46 +0530

    Readjusted viewwidth of Video and Homepage

--- src/components/Homepage/homePage.jsx
+++ src/components/Homepage/homePage.jsx
@@ -41,6 +41,97 @@ const HomePage = ({sideNavbar}) => {
                 <div className="youtubeTitleBoxProfile">
                     <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
                 </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
+            </div>
+        </div>
+        <div className="youtube_Video"> {/* Single thumbnail*/ } 
+            <div className="youtube_thumbnailBox">
+                <img src="https://i.ytimg.com/vi/UN-F5ZWwwJc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARTiM0ALKqtyvQkEpLUL1KEUUQAg" className="youtube_thumbnailPic" alt="Thumbnail"/>
+                <div className="youtube_timingThumbnail">
+                    28:05
+                </div>
+            </div>
+            <div className="youtubeTitleBox"> {/* Consists username, dp, video title and views*/ }
+                <div className="youtubeTitleBoxProfile">
+                    <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
+                </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
+            </div>
+        </div>        <div className="youtube_Video"> {/* Single thumbnail*/ } 
+            <div className="youtube_thumbnailBox">
+                <img src="https://i.ytimg.com/vi/UN-F5ZWwwJc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARTiM0ALKqtyvQkEpLUL1KEUUQAg" className="youtube_thumbnailPic" alt="Thumbnail"/>
+                <div className="youtube_timingThumbnail">
+                    28:05
+                </div>
+            </div>
+            <div className="youtubeTitleBox"> {/* Consists username, dp, video title and views*/ }
+                <div className="youtubeTitleBoxProfile">
+                    <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
+                </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
+            </div>
+        </div>        <div className="youtube_Video"> {/* Single thumbnail*/ } 
+            <div className="youtube_thumbnailBox">
+                <img src="https://i.ytimg.com/vi/UN-F5ZWwwJc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARTiM0ALKqtyvQkEpLUL1KEUUQAg" className="youtube_thumbnailPic" alt="Thumbnail"/>
+                <div className="youtube_timingThumbnail">
+                    28:05
+                </div>
+            </div>
+            <div className="youtubeTitleBox"> {/* Consists username, dp, video title and views*/ }
+                <div className="youtubeTitleBoxProfile">
+                    <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
+                </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
+            </div>
+        </div>        <div className="youtube_Video"> {/* Single thumbnail*/ } 
+            <div className="youtube_thumbnailBox">
+                <img src="https://i.ytimg.com/vi/UN-F5ZWwwJc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARTiM0ALKqtyvQkEpLUL1KEUUQAg" className="youtube_thumbnailPic" alt="Thumbnail"/>
+                <div className="youtube_timingThumbnail">
+                    28:05
+                </div>
+            </div>
+            <div className="youtubeTitleBox"> {/* Consists username, dp, video title and views*/ }
+                <div className="youtubeTitleBoxProfile">
+                    <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
+                </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
+            </div>
+        </div>        <div className="youtube_Video"> {/* Single thumbnail*/ } 
+            <div className="youtube_thumbnailBox">
+                <img src="https://i.ytimg.com/vi/UN-F5ZWwwJc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARTiM0ALKqtyvQkEpLUL1KEUUQAg" className="youtube_thumbnailPic" alt="Thumbnail"/>
+                <div className="youtube_timingThumbnail">
+                    28:05
+                </div>
+            </div>
+            <div className="youtubeTitleBox"> {/* Consists username, dp, video title and views*/ }
+                <div className="youtubeTitleBoxProfile">
+                    <img src="https://yt3.ggpht.com/8jshuX4Pme6Q_h_HWwR1EMz7nAoZKYayugfh055Y1jxx0Ne5BJHPaQBfnRUZ-TWYbqaAhD4nLRY=s88-c-k-c0x00ffffff-no-rj" alt="profile" className="youtube_thumbnail_Profile" />
+                </div>
+                <div className="youtubeTitleBox_Title">
+                    <div className="youtube_videoTitle">Dummy User</div>
+                    <div className="youtube_channelName">Nerd's lesson</div>
+                    <div className="youtubeVideo_views">24 likes</div>
+                </div>
             </div>
         </div>
         
