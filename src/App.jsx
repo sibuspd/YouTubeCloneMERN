ceb6255e09c53c086e02b2f0f004afc927b9f137
sibuspd <sibuspd@gmail.com>
2024-12-09 20:36:08 +0530

    Router Configuration Created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/navbar'
 import Home from './pages/Home/home'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <div className='App'>
         <Navbar sideNavbarOnClick= {sideNavbarOnClick} sideNavbar={sideNavbar}/> 
         <Home sideNavbar = {sideNavbar}/>
+        <Outlet/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import HomePage from './components/Homepage/homePage.jsx';
+
+//Router Configuration 
+
+const appRouter = createBrowserRouter([{
+  path:'/',
+  element: <App/>,
+  children: [
+    {
+      path: '/',
+      element: <HomePage/>
+    }
+  ]
+}]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={appRouter}/>
   </StrictMode>,
 )
