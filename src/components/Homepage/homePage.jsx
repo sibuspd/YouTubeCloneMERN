6a44d7c4e5a8816be43f8c00a3abc6c139743e44
sibuspd <sibuspd@gmail.com>
2024-12-09 14:37:39 +0530

    Fixed the horizontal scrolling issue on Filters

--- src/components/Homepage/homePage.jsx
+++ src/components/Homepage/homePage.jsx
@@ -1,9 +1,35 @@
-import "./homePage.css"
+import "./homePage.css";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  //HOMEPAGE = FilterOptions + Video Thumbnails
 
-export default HomePage
+  const filters = [
+    "All",
+    "Twenty20 Cricket",
+    "Coding",
+    "Machine Learning",
+    "Music",
+    "Hindustani Classical",
+    "Live",
+    "Mixes",
+    "Gaming",
+    "Debates",
+    "Coke Studio Pakistan",
+    "Democracy",
+    "Pakistani Dramas",
+    "Comedy",
+  ];
+
+  return (
+    <div className="homepage">
+      <div className="homepage_options">
+        {filters.map((category, index) => {
+          return <div className="homepage_option" key={index}>{category}</div>;
+        })}
+        <div className="content_below_filters"> skyfall </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
