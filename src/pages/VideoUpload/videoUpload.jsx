5086b44961fcb09479eecb3a69f28d5d1ddba7cd
sibuspd <sibuspd@gmail.com>
2024-12-10 21:10:17 +0530

    Video Upload Module Updated

--- src/pages/VideoUpload/videoUpload.jsx
+++ src/pages/VideoUpload/videoUpload.jsx
@@ -1,8 +1,18 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./videoUpload.css"
 import YouTubeIcon from '@mui/icons-material/YouTube';
     
     const VideoUpload = () => {
+
+        const [inputField, setInputField] = useState({"title":"", "description":"", "videolink":"", "thumbnail":"", "videoType":""});
+
+        const handleOnChangeInput = (event, name) =>{
+            setInputField({
+                ...inputField,[name]:event.target.value // Computed Value
+            })
+        }
+
       return (
         <div className="videoUpload">
             <div className="uploadBox">
@@ -12,9 +22,9 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
                 </div>
 
                 <div className="uploadForm">
-                    <input type="text" className="uploadFormInputs" placeholder="Title of the Video" />
-                    <input type="text" className="uploadFormInputs" placeholder="Description" />
-                    <input type="text" className="uploadFormInputs" placeholder="Category" />
+                    <input type="text" className="uploadFormInputs" placeholder="Title of the Video" value={inputField.title} onChange={(e)=> handleOnChangeInput(e, "title")}/>
+                    <input type="text" className="uploadFormInputs" placeholder="Description" value={inputField.description} onChange={(e)=> handleOnChangeInput(e, "description")} />
+                    <input type="text" className="uploadFormInputs" placeholder="Category" value={inputField.videoType}  onChange={(e)=> handleOnChangeInput(e, "videoType")}/>
                     <div>Thumbnail <input type="file" accept="image/*" /> {/**Accepts only image */}</div>
                     <div>Video <input type="file" accept="video/mp4" /> {/**Accepts only video files */}</div>
                 </div>
